ac40a7c27f98ca8b1bda3eb7386cfdaeba260067
DuMOHsmol <dumohsmol@yandex.ru>
2012-01-29 01:51:38 +0400

    Ver 2.3. Reverted back to source from 58 firmware. Updated version number in ramdisks to 62. Reverted old frequency table with small fix. Enabled usb-storage, so you dont need usb-storage.ko now. Enabled ntfs support. Salvaged some ram. Now version number shows actual version of kernel (#23=2.3).

--- build-all.sh
+++ build-all.sh
@@ -1,4 +1,12 @@
+version=23
+
+
+version=`expr $version - 1`
+echo $version > ./kernel/.version
 ./build-minipro.sh
+echo $version > ./kernel/.version
 ./build-mini.sh
+echo $version > ./kernel/.version
 ./build-lww.sh
+echo $version > ./kernel/.version
 ./build-active.sh
--- upload-to-git.sh
+++ upload-to-git.sh
@@ -1,3 +1,3 @@
 git add ./*
-git commit -m 'testing upload'
+git commit -m 'Ver 2.3. Reverted back to source from 58 firmware. Updated version number in ramdisks to 62. Reverted old frequency table with small fix. Enabled usb-storage, so you dont need usb-storage.ko now. Enabled ntfs support. Salvaged some ram. Now version number shows actual version of kernel (#23=2.3).'
 git push -u origin master
