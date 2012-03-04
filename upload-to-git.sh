3f5b2b239833fd8d6a2b59e9d75f1cdde417e474
DuMOHsmol <dumohsmol@yandex.ru>
2012-03-04 20:22:04 +0400

    Ver 2.6, removed lulzactive governor (it wasnt working properly).

--- upload-to-git.sh
+++ upload-to-git.sh
@@ -1,3 +1,3 @@
 git add ./*
-git commit -m 'Ver 2.6, removed lulzactive governor (i wasnt working properly).'
+git commit -m 'Ver 2.6, removed lulzactive governor (it wasnt working properly).'
 git push -u origin master
